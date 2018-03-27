5bade39af4978be2a3df9501aea25901d79ea722
Rohan Shukla <voidrohan@gmail.com>
2018-03-27 17:29:06 +0530

    second commit

---
+++ commandline.h
@@ -0,0 +1,27 @@
+#ifndef COMMANDLINE_H
+#define COMMANDLINE_H
+
+#include <QObject>
+#include <QThread>
+#include <QDebug>
+#include <QTextStream>
+
+class CommandLine : public QObject
+{
+    Q_OBJECT
+public:
+    explicit CommandLine(QObject *parent = 0);
+
+signals:
+    void onReadLine(QString Line);
+
+public slots:
+
+    void ReadSTDIN();
+    void HandleSTDIN(QString Line);
+
+ private:
+    QThread mThread;
+};
+
+#endif // COMMANDLINE_H
